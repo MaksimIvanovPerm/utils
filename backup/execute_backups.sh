806823d5eed5c42f7a50a14b8b64b48f3435bab0
Maksim <35660055+MaksimIvanovPerm@users.noreply.github.com>
2021-10-24 20:57:23 +0500

    Create execute_backups.sh

---
+++ backup/execute_backups.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#as an example:
+#./dobackup.sh -w "/" -s "/cygdrive/d/backup/cygwin" -c
+#./dobackup.sh -w "/cygdrive/c/Users/Documents/toadscripts" -s "/cygdrive/d/backup/toadscripts" -c -e "none"
